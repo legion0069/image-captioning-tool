88690cf679c3959f8abbd0203599d0f858794b1b
SAI TEJA VERUKONDA <152319571+legion0069@users.noreply.github.com>
2025-12-02 22:48:53 +0530

    Delete src directory

--- src/config.py
+++
@@ -1,6 +0,0 @@
-from pathlib import Path
-
-BASE_DIR = Path(__file__).resolve().parent.parent
-DATA_DIR = BASE_DIR / "data"
-MODELS_DIR = BASE_DIR / "models"
-SAMPLE_IMAGES_DIR = BASE_DIR / "sample_images"
